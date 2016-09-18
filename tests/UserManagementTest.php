e133364d6fab1c51089039b34f64ed778ed077d5
Tim Joosten <Tjoosten3@gmail.com>
2016-09-18 19:23:26 +0200

    Add empty test classes

---
+++ tests/UserManagementTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserManagementTest extends TestCase
+{
+    use DatabaseMigrations, DatabaseTransactions;
+
+    /**
+     *
+     */
+    public function testOverview()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function testCreateView()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function testCreateMethodWithError()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function testCreateMeethodWithoutErrors()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function testDeleteMethod()
+    {
+
+    }
+}
