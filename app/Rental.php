147d8a6936f81de96faa188e147e6d59c5a01608
Tjoosten <Tjoosten3@gmail.com>
2016-09-25 09:50:22 +0200

    Permission fix

--- app/Rental.php
+++ app/Rental.php
@@ -4,7 +4,33 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Rental
+ * @package App
+ */
 class Rental extends Model
 {
-    //
+    /**
+     * MAss-assign fields
+     *
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * Hidden fields
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * Status relation to indicate the rental status.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function status()
+    {
+        return $this->belongsTo('App\Status');
+    }
 }
