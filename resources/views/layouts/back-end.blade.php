a440662f6239ab4cc7ac330fa8cfa16b6f28af76
Tim Joosten <Tjoosten3@gmail.com>
2016-09-25 07:38:01 +0200

    change backend heading

--- resources/views/layouts/back-end.blade.php
+++ resources/views/layouts/back-end.blade.php
@@ -28,9 +28,9 @@
             {{-- Logo --}}
             <a href="../../index2.html" class="logo">
                 {{-- mini logo for sidebar mini 50x50 pixels --}}
-                <span class="logo-mini"><b>A</b>LT</span>
+                <span class="logo-mini"><b>S</b></span>
                 {{-- logo for regular state and mobile devices --}}
-                <span class="logo-lg"><b>Admin</b>LTE</span>
+                <span class="logo-lg"><b>S</b>ijot</span>
             </a>
             {{-- Header Navbar: style can be found in header.less --}}
             <nav class="navbar navbar-static-top">
