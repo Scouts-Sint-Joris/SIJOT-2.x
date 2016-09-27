82bb8434d699a57b322bad3b6d486dc69e2b89bb
Tim Joosten <Tjoosten3@gmail.com>
2016-09-27 08:07:48 +0200

    Add rental model

--- app/Http/Requests/RentalValidator.php
+++ app/Http/Requests/RentalValidator.php
@@ -13,7 +13,7 @@ class RentalValidator extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Rental.php
+++ app/Rental.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rental extends Model
 {
-    //
+    /**
+     * Set the attributes that should be mutated to dates. 
+     * 
+     * @var array 
+     */
+    protected $dates = ['end_date', 'start_date'];
+    
+    /**
+     * Mass assign-fields.
+     *
+     * @var array
+     */
+    protected $fillable = ['start_date', 'end_date', 'group', 'phone_number', 'email'];
+
+    /**
+     * Hidden fields. 
+     * 
+     * @var array
+     */
+    protected $hidden = ['updated_at', 'created_at'];
 }
