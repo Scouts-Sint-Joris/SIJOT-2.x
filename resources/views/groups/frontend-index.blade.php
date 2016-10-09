1525322f799ac477f160efde3881511f0971609d
Tim Joosten <Tjoosten3@gmail.com>
2016-10-10 01:10:33 +0200

    Add groups index barebone

--- resources/views/groups/frontend-index.blade.php
+++ resources/views/groups/frontend-index.blade.php
@@ -13,6 +13,91 @@
                 <div class="panel-body">
                     <div class="col-md-8">
                         <h2>Takken:</h2>
+
+                        <div style="border-radius: 8px;" class="well well-sm color-kapoenen">
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#">
+                                        <img class="media-object color-white" style="height: 75px; width: 75px;" src="{{ asset('assets/img/kapoenen_zeehondjes.svg') }}" alt="...">
+                                    </a>
+                                </div>
+                                <div class="media-body color-white">
+                                    <h4 class="media-heading">Media heading</h4>
+                                    ...
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="border-radius: 8px;" class="well well-sm color-welpen">
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#">
+                                        <img class="media-object color-white" style="height: 75px; width: 75px;" src="{{ asset('assets/img/zeekabouters_welpen.svg') }}" alt="...">
+                                    </a>
+                                </div>
+                                <div class="media-body color-white">
+                                    <h4 class="media-heading">Media heading</h4>
+                                    ...
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="border-radius: 8px;" class="well well-sm color-jonggivers">
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#">
+                                        <img class="media-object color-white" style="height: 75px; width: 75px;" src="{{ asset('assets/img/jonggidsen_jongverkenners_scheepsmakkers.svg') }}">
+                                    </a>
+                                </div>
+                                <div class="media-body color-white">
+                                    <h4 class="media-heading">Media heading</h4>
+                                    ....
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="border-radius: 8px;" class="well well-sm color-givers">
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#">
+                                        <img class="media-object color-white" style="height: 75px; width: 75px;" src="{{ asset('assets/img/zee_gidsen_verkenners.svg') }}">
+                                    </a>
+                                </div>
+                                <div class="media-body color-white">
+                                    <h4 class="media-heading">Media heading</h4>
+                                    ....
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="border-radius: 8px;" class="well well-sm color-jins">
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#">
+                                        <img class="media-object color-white" style="height: 75px; width: 75px;" src="{{ asset('assets/img/jins_loodsen.svg') }}">
+                                    </a>
+                                </div>
+                                <div class="media-body color-white">
+                                    <h4 class="media-heading">Media heading</h4>
+                                    ....
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="border-radius: 8px;" class="well well-sm color-leiding">
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#">
+                                        <img class="media-object color-white" style="height: 75px; width: 75px;" src="{{ asset('assets/img/leiding.svg') }}">
+                                    </a>
+                                </div>
+                                <div class="media-body color-white">
+                                    <h4 class="media-heading">Media heading</h4>
+                                    ....
+                                </div>
+                            </div>
+                        </div>
+
                     </div>    
                 </div>
             </div>
