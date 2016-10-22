18f5fddd5dccf1f68173a453c460cd702f14fcf9
Dominic Jones <haakym@gmail.com>
2016-10-22 09:56:33 +0100

    blade markup, remove static mockup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,36 +11,23 @@
         <div class="col-sm-12">
             <div style="border-radius:0px; border: 0px;" class="panel panel-default">
                 <div class="panel-body">
-                    <div class="col-md-8">
-                        <h3>Alice in Wonderland, part dos</h3>
-                        <p>
-                            'You ought to be ashamed of yourself for asking such a simple question,' added the Gryphon; and then they both sat silent and looked at poor Alice, who felt ready to sink into the earth. At last the Gryphon said to the Mock Turtle, 'Drive on, old fellow! Don't be all day about it!' and he went on in these words:
-                            'Yes, we went to school in the sea, though you mayn't believe it�'
-                            'I never said I didn't!' interrupted Alice.
-                            'You did,' said the Mock Turtle.
-                        </p>
-                        <div>
-                            <span class="badge">Posted 2012-08-02 20:47:04</span><div class="pull-right">
-                                <span class="label label-default">alice</span> <span class="label label-primary">story</span> <span class="label label-success">blog</span> <span class="label label-info">personal</span> <span class="label label-warning">Warning</span>
-                                <span class="label label-danger">Danger</span>
-                            </div>
-                        </div>
-                        <hr>
-                        <h3>Revolution has begun!</h3>
-                        <p>
-                            'I am bound to Tahiti for more men.'
-                            'Very good. Let me board you a moment�I come in peace.' With that he leaped from the canoe, swam to the boat; and climbing the gunwale, stood face to face with the captain.
-                            'Cross your arms, sir; throw back your head. Now, repeat after me. As soon as Steelkilt leaves me, I swear to beach this boat on yonder island, and remain there six days. If I do not, may lightning strike me!'A pretty scholar,' laughed the Lakeman. 'Adios, Senor!' and leaping into the sea, he swam back to his comrades.
-                        </p>
-                        <div>
-                            <span class="badge">Posted 2012-08-02 20:47:04</span><div class="pull-right">
-                                <span class="label label-default">alice</span> <span class="label label-primary">story</span> <span class="label label-success">blog</span> <span class="label label-info">personal</span> <span class="label label-warning">Warning</span>
-                                <span class="label label-danger">Danger</span>
-                            </div>
-                        </div>
+                    <div class="col-md-8" id="news">
+                        @foreach($news as $article)
+                            <article>
+                                <h3>{{ $article->heading }}</h3>
+                                <p>{{ $article->content }}</p>
+                                <div>
+                                    <span class="badge">Posted {{ $article->created_at->format('Y-m-d H:i:s') }}</span>
+                                    <div class="pull-right">
+                                        @foreach($article->tags as $tag)
+                                            <span class="{{ $tag->class }}">{{ $tag->name }}</span>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            </article>
+                        @endforeach
                         <hr>
                     </div>
-
                     <div class="col-md-4">
                         <div class="panel panel-default">
                             <div class="panel-heading">Activiteiten:</div>
