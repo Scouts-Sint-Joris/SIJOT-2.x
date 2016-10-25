ebe13ee715a96d79f6df051f4e4f07045a8b91fe
Tim Joosten <Tjoosten3@gmail.com>
2016-10-25 03:08:18 +0200

    Add section to the rental lang file.

---
+++ resources/lang/en/rental.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /**
+     * --------------------------------------------------------------------------
+     * Rental Language Lines
+     * --------------------------------------------------------------------------
+     *
+     * The following language lines are used by rental system.
+     * Her you can modify these lines.
+     *
+     */
+
+    // Back-end overview table.
+
+
+];
