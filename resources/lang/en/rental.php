d74a5c2237d20e7e7d5667354a4d55751f8224b5
Tim Joosten <Tjoosten3@gmail.com>
2016-10-26 02:04:10 +0200

    Fix #47 #48

--- resources/lang/en/rental.php
+++ resources/lang/en/rental.php
@@ -13,22 +13,26 @@ return [
      */
 
     // Header and breadcrumb.
-    'lease-export'       => 'Export',
-    'lease-add'          => 'Add lease',
-    'lease-option'       => 'Option',
-    'lease-new'          => 'New lease',
-    'lease-delete'       => 'Delete',
-    'lease-confirm'      => 'Confirm',
+    'lease-export'           => 'Export',
+    'lease-add'              => 'Add lease',
+    'lease-option'           => 'Option',
+    'lease-new'              => 'New lease',
+    'lease-delete'           => 'Delete',
+    'lease-confirm'          => 'Confirm',
 
     // Back-end overview table headers.
-    'lease-start-date'   => 'Start date:',
-    'lease-end-date'     => 'End date:',
-    'lease-status'       => 'Status:',
-    'lease-group'        => 'Group:',
-    'lease-create-date'  => 'Creation date:',
-    'lease-email'        => 'Email:',
-    'lease-phone-number' => 'Phone number:'
+    'lease-start-date'       => 'Start date:',
+    'lease-end-date'         => 'End date:',
+    'lease-status'           => 'Status:',
+    'lease-group'            => 'Group:',
+    'lease-create-date'      => 'Creation date:',
+    'lease-email'            => 'Email:',
+    'lease-phone-number'     => 'Phone number:',
 
     // Back-end overview labels.
+    'lease-breadcrumb-index' => 'Home',
+    'lease-header-box'       => 'Lease overview',
+    'lease-title'            => 'Leases',
+    'lease-sub-title'        => 'Leases module for the website.',
 
 ];
--- resources/views/rental/backend-overview.blade.php
+++ resources/views/rental/backend-overview.blade.php
@@ -2,19 +2,28 @@
 
 @section('content-header')
 <h1>
-    Verhuur.
-    <small>Verhuur module voor het domein.</small>
+    {{ trans('rental.lease-title') }}.
+    <small>{{ trans('rental.lease-sub-title') }}</small>
 </h1>
 <ol class="breadcrumb">
-    <li><a href="{{ url('/') }}"><i class="fa fa-dashboard"></i> Home</a></li>
-    <li class="active">Verhuur</li>
+    <li>
+        <a href="{{ url('/') }}">
+            <i class="fa fa-dashboard"></i>
+            {{ trans('rental.lease-breadcrumb-index') }}
+        </a>
+    </li>
+    <li class="active">
+        {{ trans('rental.lease-title') }}
+    </li>
 </ol>
 @endsection 
 
 @section('content')
 <div class="box">
     <div class="box-header with-border">
-        <h3 class="box-title">Verhuur overzicht</h3>
+        <h3 class="box-title">
+            {{ trans('rental.lease-header-box') }}
+        </h3>
 
         <div class="pull-right">
             <a href="{{ route('rental.backend.export') }}" class="btn btn-danger btn-xs">
