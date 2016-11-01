18f05911dac742b513ac8d03fc3cf012761a4d74
Tjoosten <Tjoosten3@gmail.com>
2016-11-01 20:33:42 +0100

    Update sidebar

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -2,11 +2,11 @@
 
 @section('content-header')
 <h1>
-    Account configuratie <small>Configuratie module voor uw profiel..</small>
+    User profile <!-- <small></small> -->
 </h1>
 <ol class="breadcrumb">
     <li><a href="{{ url('/') }}"><i class="fa fa-dashboard"></i> Home</a></li>
-    <li class="active">Account configuratie</li>
+    <li class="active">Account</li>
 </ol>
 @endsection
 
@@ -17,25 +17,24 @@
                 <!-- Profile Image -->
                 <div class="box box-primary">
                     <div class="box-body box-profile">
-                        <img class="profile-user-img img-responsive img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+                        @if (file_exists(public_path(auth()->user()->avatar)) && auth()->user()->avatar !== null)
+                            <img class="profile-user-img img-responsive img-circle" src="{{ asset(auth()->user()->avatar) }}" alt="{{ auth()->user()->name }}">
+                        @else
+                            <img src="{{ asset('assets/avatars/default.png') }}" class="profile-user-img img-responsive img-circle" alt="{{ auth()->user()->name }}">
+                        @endif
 
                         <h3 class="profile-username text-center">{{ $user->name }}</h3>
 
-                        <p class="text-muted text-center">Software Engineer</p>
-
                         <ul class="list-group list-group-unbordered">
                             <li class="list-group-item">
-                                <b>Followers</b> <a class="pull-right">1,322</a>
+                                <b>Creatie:</b> <span class="pull-right">{{ $user->created_at }}</span>
                             </li>
                             <li class="list-group-item">
-                                <b>Following</b> <a class="pull-right">543</a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>Friends</b> <a class="pull-right">13,287</a>
+                                <b>Gewijzigd:</b> <span class="pull-right">{{ $user->updated_at }}</span>
                             </li>
                         </ul>
 
-                        <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+                        <a href="#" class="btn btn-primary btn-block"><b>Contact</b></a>
                     </div>
                     <!-- /.box-body -->
                 </div>
