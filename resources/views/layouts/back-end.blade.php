e4e0841b42b41e0514d6775607c683118af63bc9
Tjoosten <Tjoosten3@gmail.com>
2016-11-06 05:18:40 +0100

    Update

--- resources/views/layouts/back-end.blade.php
+++ resources/views/layouts/back-end.blade.php
@@ -48,26 +48,37 @@
                     <div class="navbar-custom-menu">
                         <ul class="nav navbar-nav">
                             {{-- Notifications: style can be found in dropdown.less --}}
-                            <li class="dropdown notifications-menu">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-bell-o"></i>
-                                    <span class="label label-warning">10</span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="header">You have 10 notifications</li>
-                                    <li>
-                                        <!-- inner menu: contains the actual data -->
-                                        <ul class="menu">
-                                            <li>
-                                                <a href="#">
-                                                    <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li class="footer"><a href="#">View all</a></li>
-                                </ul>
-                            </li>
+                            @if(count(auth()->user()->unreadNotifications) === 0)
+                                <li class="dropdown notifications-menu">
+                                    <a href="">
+                                        <i class="fa fa-bell-o"></i>
+                                        <span class="label label-warning">
+                                            {{ count(auth()->user()->unreadNotifications) }}
+                                        </span>
+                                    </a>
+                                </li>
+                            @else
+                                <li class="dropdown notifications-menu">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                        <i class="fa fa-bell-o"></i>
+                                        <span class="label label-warning">{{ count(auth()->user()->unreadNotifications) }}</span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li class="header">You have {{ count(auth()->user()->unreadNotifications) }} notifications</li>
+                                        <li>
+                                            <!-- inner menu: contains the actual data -->
+                                            <ul class="menu">
+                                                <li>
+                                                    <a href="#">
+                                                        <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="footer"><a href="#">View all</a></li>
+                                    </ul>
+                                </li>
+                            @endif
 
                             {{-- User Account: style can be found in dropdown.less --}}
                             <li class="user user-menu">
