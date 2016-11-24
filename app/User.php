218ecf3dc9ada1e863a332f68d00969c683e6a44
Tim Joosten <Tjoosten3@gmail.com>
2016-11-24 05:58:42 +0100

    Fix violations in app/User.php

--- app/User.php
+++ app/User.php
@@ -31,7 +31,7 @@ class User extends Authenticatable
     protected $hidden = ['password', 'remember_token'];
 
     /**
-     * Check if the user is logged in. 
+     * Check if the user is logged in.
      * --------
      * USAGE: auth()->user()->isOnline();
      *
@@ -42,7 +42,10 @@ class User extends Authenticatable
         return Cache::has('user-is-online-' . $this->id);
     }
 
-    public function isActive() {
-        return in_array('active', $this->permissions()->get()->map(function ($p) {return $p->name;}));
+    public function isActive()
+    {
+        return in_array('active', $this->permissions()->get()->map(function ($p) {
+            return $p->name;
+        }));
     }
 }
