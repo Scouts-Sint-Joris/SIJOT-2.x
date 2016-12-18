59e830bade4a3c7174f22a056962749d7fe36b46
Tim Joosten <Tjoosten3@gmail.com>
2016-12-19 00:31:03 +0100

    Update docblocks

--- tests/ApiKeyManagementTest.php
+++ tests/ApiKeyManagementTest.php
@@ -8,43 +8,83 @@ class ApiKeyManagementTest extends TestCase
 {
     use DatabaseMigrations, DatabaseTransactions;
 
+    /**
+     * @group all
+     * @group api
+     */
     public function testCreateKeyWithErrors()
     {
-
+        $apiKey = factory(ApiKey::class)->create();
+        $headers['X-Authorization'] = $apiKey->key;
     }
 
+    /**
+     * @group all
+     * @group api
+     */
     public function testCreateKeyWithoutErrors()
     {
-
+        $apiKey = factory(ApiKey::class)->create();
+        $headers['X-Authorization'] = $apiKey->key;
     }
 
+    /**
+     * @group all
+     * @group api
+     */
     public function testRegenerateKey()
     {
-
+        $apiKey = factory(ApiKey::class)->create();
+        $headers['X-Authorization'] = $apiKey->key;
     }
 
+    /**
+     * @group all
+     * @group api
+     */
     public function testRegenerateKeyWithoutValidId()
     {
-
+        $apiKey = factory(ApiKey::class)->create();
+        $headers['X-Authorization'] = $apiKey->key;
     }
 
+    /**
+     * @group all
+     * @group api
+     */
     public function testUpdateKeyWithErrors()
     {
-
+        $apiKey = factory(ApiKey::class)->create();
+        $headers['X-Authorization'] = $apiKey->key;
     }
 
+    /**
+     * @group all
+     * @group api
+     */
     public function testUpdateKeyWithoutErrors()
     {
-
+        $apiKey = factory(ApiKey::class)->create();
+        $headers['X-Authorization'] = $apiKey->key;
     }
 
+    /**
+     * @group all
+     * @group api
+     */
     public function testDeleteKey()
     {
-
+        $apiKey = factory(ApiKey::class)->create();
+        $headers['X-Authorization'] = $apiKey->key;
     }
 
+    /**
+     * @group all
+     * @group api
+     */
     public function testDeleteKeyWithoutValidId()
     {
-
+        $apiKey = factory(ApiKey::class)->create();
+        $headers['X-Authorization'] = $apiKey->key;
     }
 }
